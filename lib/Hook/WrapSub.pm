0223a9f0b7e0eb71f8a10995933a8eacd1f1316e
Neil Bowers <neil@bowers.com>
2014-06-18 22:49:00 +0100

    Added SEE ALSO section

--- lib/Hook/WrapSub.pm
+++ lib/Hook/WrapSub.pm
@@ -287,6 +287,42 @@ sub unwrap_subs(@) {
 
 1;
 
+=head1 SEE ALSO
+
+L<Hook::LexWrap> provides a similar capability to C<Hook::WrapSub>,
+but has the benefit that the C<caller()> function works correctly
+within the wrapped subroutine.
+
+L<Sub::Prepend> lets you provide a sub that will be called before
+a named sub. The C<caller()> function works correctly in the
+wrapped sub.
+
+L<Sub::Mage> provides a number of related functions.
+You can provide pre- and post-call hooks,
+you can temporarily override a function and then restore it later,
+and more.
+
+L<Class::Hook> lets you add pre- and post-call hooks around any
+methods called by your code. It doesn't support functions.
+
+L<Hook::Scope> lets you register callbacks that will be invoked
+when execution leaves the scope they were registered in.
+
+L<Hook::PrePostCall> provides an OO interface for wrapping
+a function with pre- and post-call hook functions.
+Last updated in 1997, and marked as alpha.
+
+L<Hook::Heckle> provides an OO interface for wrapping pre- and post-call
+hooks around functions or methods in a package. Not updated sinc 2003,
+and has a 20% failed rate on CPAN Testers.
+
+L<Class::Wrap> provides the C<wrap()> function, which takes a coderef
+and a package name. The coderef is invoked every time a method in
+the package is called.
+
+L<Sub::Versive> lets you stack pre- and post-call hooks.
+Last updated in 2001.
+
 =head1 AUTHOR
 
 jdporter@min.net (John Porter)
